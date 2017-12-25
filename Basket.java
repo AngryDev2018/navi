e5ad4538f0d3559580e38ef9441156549c084830
IteaUser <IteaUser@ukr.net>
2017-12-25 20:30:31 +0200

    Vvvv

--- Basket.java
+++ Basket.java
@@ -1,18 +1,5 @@
 
 
 public class Basket {
-	String name;
-	Goods[] goods=new Goods[10];
-	Basket(String name,Goods[] goods){
-		
-		this.name=name;
-		this.goods=goods;
-	}
-	void getGoods(){
-		for(Goods a:goods){
-			a.getGoods();
-		}
-	}
-	
-	Goods[] getGoogsArr(){return goods;}
+
 }
